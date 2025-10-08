37d5f0a5b2aa0a1ff39e81b54d70f3d591a4c650
lijunxi <2770063826@qq.com>
2025-10-08 20:56:42 +0800

    fix:细节完善

--- easyadmin-web/src/router/index.js
+++ easyadmin-web/src/router/index.js
@@ -78,7 +78,7 @@ export const constantRoutes = [
       {
         path: 'dashboard',
         component: () => import('@/views/dashboard/index'),
-        name: 'Dashboard',
+        name: '仪表盘',
         meta: { title: '仪表盘', icon: 'dashboard', affix: true }
       }
     ]
